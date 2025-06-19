3ae38378a3fdff75d76b2f8ec0a0543dce7d1358
mxmlnwbr <maximilian.weber@bluewin.ch>
2025-06-19 08:07:48 +0200

    feat: implement text classification using KoalaAI/Text-Moderation model

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def main():
-    print("Hello from lakera-coding-challenge!")
+# Use a pipeline as a high-level helper
+from transformers import pipeline
 
+def main():
+    pipe = pipeline("text-classification", model="KoalaAI/Text-Moderation")
+
+    result = pipe("I love you!")
+    print(result)
 
 if __name__ == "__main__":
     main()
