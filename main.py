6a6596a55c05d3b93616a507cabb8859541e8d48
mxmlnwbr <maximilian.weber@bluewin.ch>
2025-06-19 08:18:16 +0200

    feat: display all classification scores with labels instead of raw output

--- main.py
+++ main.py
@@ -4,8 +4,12 @@ from transformers import pipeline
 def main():
     pipe = pipeline("text-classification", model="KoalaAI/Text-Moderation")
 
-    result = pipe("I love you!")
-    print(result)
+    result = pipe("I love you!", top_k=None)
+    
+    # print result with score
+    for prediction in result:
+        print(f"{prediction['label']}: {prediction['score']}")
+
 
 if __name__ == "__main__":
     main()
